c0112b346ab566ad8c51f8cc58aad45e9c18f3cf
Renato <ren811@gmail.com>
2017-07-18 19:15:16 -0300

    Added a simple test.

---
+++ tests.py
@@ -0,0 +1,23 @@
+"""requets module and requets mocks"""
+import unittest
+import requests_mock
+import fixer
+
+# # pylint: disable=invalid-name
+class TestRequetsParsing(unittest.TestCase):
+    """Test parsing from webservice"""
+    def test_latest(self):
+        """"""
+        with requests_mock.Mocker() as m:
+            api_base_url = 'http://api.fixer.io/'
+            m.register_uri('GET',
+                           api_base_url + 'latest?base=USD',
+                           text='{"status":200,"rates":{"EUR":0.8}}')
+            rates = fixer.get_rates('USD')
+            
+            self.assertNotEqual(None, rates['rates'])
+            self.assertNotEqual(None, rates['rates']['EUR'])
+            self.assertEqual(0.8, rates['rates']['EUR'])
+
+if __name__ == '__main__':
+    unittest.main()
