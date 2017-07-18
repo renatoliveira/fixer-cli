eaeb623e3e97611710b1df24428e02f5cf40ff7e
Renato <ren811@gmail.com>
2017-07-18 19:16:05 -0300

    Fixed some pylint errors.

--- tests.py
+++ tests.py
@@ -3,18 +3,17 @@ import unittest
 import requests_mock
 import fixer
 
-# # pylint: disable=invalid-name
+# pylint: disable=invalid-name
 class TestRequetsParsing(unittest.TestCase):
     """Test parsing from webservice"""
     def test_latest(self):
-        """"""
+        """Test getting the latest currency"""
         with requests_mock.Mocker() as m:
             api_base_url = 'http://api.fixer.io/'
             m.register_uri('GET',
                            api_base_url + 'latest?base=USD',
                            text='{"status":200,"rates":{"EUR":0.8}}')
             rates = fixer.get_rates('USD')
-            
             self.assertNotEqual(None, rates['rates'])
             self.assertNotEqual(None, rates['rates']['EUR'])
             self.assertEqual(0.8, rates['rates']['EUR'])
