ecfde031d03d2030677fa0b07b488053e44c122d
Renato <ren811@gmail.com>
2017-07-18 20:08:56 -0300

    Added colours, arrows, and previous rates.

--- fixer.py
+++ fixer.py
@@ -1,46 +1,54 @@
+""""Fixer"""
+from datetime import date, timedelta
 import sys
+from colorama import Fore
 import requests
 
 def get_rates(source_currency):
-    result = requests.get('http://api.fixer.io/latest?base=' + source_currency).json()
-    return result
+    """
+    Get today, yesterday and last week's rates
+    """
+    yesterday = date.today() - timedelta(1)
+    last_week = date.today() - timedelta(7)
+    latest_rates = requests.get('http://api.fixer.io/latest?base=' + source_currency).json()
+    yesterday_rates = requests.get('http://api.fixer.io/' + yesterday.strftime('%Y-%m-%d') +
+                                   '?base=' + source_currency).json()
+    last_week_rates = requests.get('http://api.fixer.io/' + last_week.strftime('%Y-%m-%d') +
+                                   '?base=' + source_currency).json()
+    return (latest_rates, yesterday_rates, last_week_rates)
 
-def print_all(r):
-    if 'error' not in r:
-        print(' - - - - - - - - - - - - - - - - - - - -')
-        for k in r['rates']:
-            print(k + ': ' + str(r['rates'][k]))
-        print(' - - - - - - - - - - - - - - - - - - - -')
-    else:
-        print(r['error'])
-
-def print_conversion(r):
-    if 'error' not in r:
-        print(' - - - - - - - - - - - - - - - - - - - -')
-        print(sys.argv[1].upper() + ' is worth approximately ' + sys.argv[2].upper() + ' ' + str(r['rates'][sys.argv[2].upper()]))
-        print(' - - - - - - - - - - - - - - - - - - - -')
-    else:
-        print(r['error'])
-
-def print_conversion_with_value(r):
-    if 'error' not in r:
-        v = float(sys.argv[3])
-        print(' - - - - - - - - - - - - - - - - - - - -')
-        print(sys.argv[1].upper() + ' ' + str(v) + ' is worth approximately ' + sys.argv[2].upper() + ' ' + str(r['rates'][sys.argv[2].upper()] * v))
-        print(' - - - - - - - - - - - - - - - - - - - -')
-    else:
-        print(r['error'])
+def print_conversion(rates):
+    """prints the conversion information"""
+    if len(rates) == 3 and 'error' not in rates[0]:
+        most_recent_rate = rates[0]['rates'][sys.argv[2].upper()]
+        yesterday_rate = rates[1]['rates'][sys.argv[2].upper()]
+        last_week_rate = rates[2]['rates'][sys.argv[2].upper()]
+        print('1 ' + sys.argv[1].upper() + ' is worth approximately ' + sys.argv[2].upper() +
+              ' ' + str(most_recent_rate) + ' today.')
+        print('')
+        if len(sys.argv) == 4:
+            val = round(float(sys.argv[3]) * most_recent_rate, 2)
+            print(sys.argv[3] + ' ' + sys.argv[1].upper() + ' is worth ' + str(val) + ' ' +
+                  sys.argv[2].upper() + ' today.')
+            print('')
+        day_diff = round((1 - (most_recent_rate / yesterday_rate)) * 100, 3)
+        week_diff = round((1 - (most_recent_rate / last_week_rate)) * 100, 3)
+        if most_recent_rate > yesterday_rate:
+            print(Fore.GREEN + '🡹 ' + Fore.RESET + str(day_diff) + '% since yesterday')
+        else:
+            print(Fore.RED + '🡻 ' + Fore.RESET + str(day_diff) + '% since yesterday')
+        if most_recent_rate > last_week_rate:
+            print(Fore.GREEN + '🡹 ' + Fore.RESET + str(week_diff) + '% since last week')
+        else:
+            print(Fore.RED + '🡻 ' + Fore.RESET + str(week_diff) + '% since last week')
+        print(Fore.RESET)
 
 def handle_options():
+    """Handle arguments passed"""
     if len(sys.argv) >= 2:
-        r = get_rates(sys.argv[1])
-    
-    if len(sys.argv) == 2:
-        print_all(r)
-    if len(sys.argv) == 3:
-        print_conversion(r)
-    if len(sys.argv) == 4:
-        print_conversion_with_value(r)
+        rates = get_rates(sys.argv[1])
+    if len(sys.argv) == 3 or len(sys.argv) == 4:
+        print_conversion(rates)
 
 if __name__ == '__main__':
     handle_options()
