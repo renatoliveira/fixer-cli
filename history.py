0dba07370aec39ec5b1303dba99005385422784d
Renato <ren811@gmail.com>
2017-07-19 21:07:51 -0300

    Added database methods and tests for it for the history.

---
+++ history.py
@@ -0,0 +1,56 @@
+import os
+import sqlite3
+
+class LocalHistory:
+    """
+    Local fixer database based on dates the user queried the API
+    """
+    def __init__(self, db_path):
+        self.db_path = db_path
+        self.connection = None
+        self.cursor = None
+        self.directory_check()
+
+    def create_tables(self, path):
+        """Creates the necessary table"""
+        self.connection = sqlite3.connect(path)
+        self.cursor = self.connection.cursor()
+        self.cursor.execute('''CREATE TABLE rates
+                          (retrieved_at DATE, tox TEXT, fromx TEXT, rate DOUBLE)''')
+        self.connection.commit()
+
+    def directory_check(self):
+        """
+        Checks whether the directory to store the history exists. If it doesn't, then create it.
+        """
+        if not os.path.exists(os.getenv('APPDATA') + '\\fixer'):
+            os.mkdir(os.getenv('APPDATA') + '\\fixer')
+        if not os.path.isfile(os.getenv('APPDATA') + '\\fixer\\history.db'):
+            self.create_tables(self.db_path)
+        else:
+            self.connection = sqlite3.connect(self.db_path)
+            self.cursor = self.connection.cursor()
+
+    def close(self):
+        """Commits and then closes the connection to sqlite3"""
+        self.connection.commit()
+        self.connection.close()
+
+    def get_rate(self, tox, fromx, when):
+        """Tries to get a currency rate conversion at a certain date"""
+        args = (tox, fromx, when)
+        self.cursor.execute('SELECT * FROM rates WHERE tox=? AND fromx=? AND retrieved_at=?', args)
+        return self.cursor.fetchone()
+
+    def get_rates(self, tox, fromx):
+        """Gets all conversions from X to Y"""
+        args = (tox, fromx)
+        self.cursor.execute('SELECT * FROM rates WHERE tox=? AND fromx=?\
+                             ORDER BY retrieved_at ASC', args)
+        return self.cursor.fetchall()
+
+    def store_rate(self, tox, fromx, when, rate):
+        """Stores the conversion for later use"""
+        args = (when, tox, fromx, rate)
+        self.cursor.execute('INSERT INTO rates VALUES (?, ?, ?, ?)', args)
+        self.connection.commit()
--- tests.py
+++ tests.py
@@ -1,8 +1,10 @@
 """requets module and requets mocks"""
+import os
 import unittest
 from datetime import date, timedelta
 import requests_mock
 import fixer
+from history import LocalHistory
 
 # pylint: disable=invalid-name
 class TestRequetsParsing(unittest.TestCase):
@@ -26,6 +28,25 @@ class TestRequetsParsing(unittest.TestCase):
             self.assertNotEqual(None, rates[0]['rates'])
             self.assertNotEqual(None, rates[0]['rates']['EUR'])
             self.assertEqual(0.8, rates[0]['rates']['EUR'])
+
+    def test_database(self):
+        """Testing the database operations"""
+        db_path = ':memory:'
+        today = date.today()
+        yesterday = date.today() - timedelta(1)
+        last_week = date.today() - timedelta(7)
+        history = LocalHistory(db_path)
+        history.store_rate('USD', 'EUR', date.today(), 0.8)
+        history.store_rate('USD', 'EUR', yesterday, 0.5)
+        history.store_rate('USD', 'EUR', last_week, 0.2)
+        today_record = history.get_rate('USD', 'EUR', today)
+        yesterday_record = history.get_rate('USD', 'EUR', yesterday)
+        last_week_record = history.get_rate('USD', 'EUR', last_week)
+        self.assertEqual(0.8, today_record[3])
+        self.assertEqual(0.5, yesterday_record[3])
+        self.assertEqual(0.2, last_week_record[3])
+        all_records = history.get_rates('USD', 'EUR')
+        self.assertEqual(3, len(all_records))
 
 if __name__ == '__main__':
     unittest.main()
